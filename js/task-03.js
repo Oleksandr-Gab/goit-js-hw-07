430d67d4d556e7f1741e9fe314efd547d3718b06
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2023-12-14 00:56:24 +0200

    2

--- js/task-03.js
+++ js/task-03.js
@@ -4,5 +4,5 @@ const span = document.querySelector('#name-output');
 
 input.addEventListener("input", (event) => {
   const inp = event.currentTarget.value;
-  inp.length > 0 && inp[0] != ' ' ? span.textContent = inp.trim() : span.textContent = 'Anonymous';
+  +inp != 0 && inp.length > 0 ? span.textContent = inp.trim() : span.textContent = 'Anonymous';
 });
